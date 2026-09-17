fe64d486a8efd4ca2a2ab5177d098a56c84f55df
Anda886 <chani04c11@gmail.com>
2026-09-17 09:17:41 +0700

    Update Examination_5.py

--- Examination_5.py
+++ Examination_5.py
@@ -1,10 +1,7 @@
-# ==========================================
-# โจทย์ข้อที่ 5: ประเมินค่าดัชนีมวลกาย (BMI)
-# Input: ค่า BMI (จำนวนจริง)
-# Output: "Underweight", "Normal", หรือ "Overweight"
-# เงื่อนไข: น้อยกว่า 18.5 พิมพ์ "Underweight" | 18.5 ถึง 22.9 พิมพ์ "Normal" | ตั้งแต่ 23 ขึ้นไป พิมพ์ "Overweight"
-# ==========================================
-
-
-# นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-bmi = float(input())
+BMI = float(input("ค่า BMI: "))
+if BMI < 18.5:
+    print("Underweight")
+elif BMI >= 22.9:
+    print("Normal")
+else:
+    print("Overweight")
